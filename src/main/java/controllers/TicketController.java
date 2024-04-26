8f7592f1de486baeff55c1bfc6d79f94e9284704
Om Mishra <om.23bcs10191@ms.sst.scaler.com>
2024-04-26 22:47:30 +0530

    final parking lot commit

---
+++ src/main/java/controllers/TicketController.java
@@ -0,0 +1,11 @@
+package controllers;
+
+import dtos.IssueTicketRequestDto;
+import dtos.IssueTicketResponseDto;
+import models.Ticket;
+
+public class TicketController {
+    public IssueTicketResponseDto issueTicket(IssueTicketRequestDto requestDto) {
+        return null;
+    }
+}
